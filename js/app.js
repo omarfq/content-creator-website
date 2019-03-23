04db931fa2b660ab2d1028ba320f8da3d155f828
Omar Flores <omarenriquefq@gmail.com>
2019-03-22 22:10:56 -0400

    Build grid structure for my-work section, added play button (still not working) on top of image

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const iframe = document.querySelector('iframe');
 const play = document.querySelector('.play');
 const player = new Vimeo.Player(iframe);
+const clickImg = document.querySelector('.click-img');
 
 play.addEventListener('click', () => {
+    clickImg.classList.add('show');
     player.play();
 });
